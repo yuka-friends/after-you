f2e87041f303fb9b94213d47b7a7312b99bc2f04
antonoko <antonoko@outlook.com>
2024-03-10 19:11:45 +0800

    add embed model check

--- afteryou/update_routine.py
+++ afteryou/update_routine.py
@@ -1,6 +1,8 @@
 import os
 import shutil
 
+from afteryou.config import config
+
 WINDOWS_FONT_FILEPATH = "C:/Windows/Fonts/"
 DEFAULT_FONT_FILEPATH = "__assets__\\LXGWWenKaiGBScreen.ttf"
 TARGET_FONT_FILEPATH = os.path.join(WINDOWS_FONT_FILEPATH, os.path.basename(DEFAULT_FONT_FILEPATH))
@@ -10,10 +12,15 @@ try:
     from afteryou import embed_manager
 
     embed_manager.get_model(mode="cpu")
+    print("   Embedding function has been enabled.")
+    config.set_and_save_config("enable_embedding", True)
 except Exception as e:
     print(e)
     print("   uform 模型似乎下载失败，请检查网络、添加代理或进行重试。")
     print("   uform model seems to have failed to download, please check the network, add a proxy, or try again.")
+    print("")
+    print("   Embedding function has been disabled.")
+    config.set_and_save_config("enable_embedding", False)
 
 
 if not os.path.exists(TARGET_FONT_FILEPATH):
