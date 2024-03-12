f8b017403597def4e48104498d3e280d95d797a5
antonoko <antonoko@outlook.com>
2024-03-12 20:36:35 +0800

    add git pull at start

--- start_app_Windows.bat
+++ start_app_Windows.bat
@@ -7,6 +7,7 @@ echo   Initializing, please stand by...
 echo.
 
 cd /d %~dp0
+git pull
 for /F "tokens=* USEBACKQ" %%A in (`python -m poetry env info --path`) do call %%A\Scripts\activate.bat
 
 streamlit run webui.py
--- start_app_macOS_linux.sh
+++ start_app_macOS_linux.sh
@@ -2,7 +2,7 @@
 echo "Initializing, please stand by..."
 
 cd "$(dirname "$0")"
-
+git pull
 source $(python3 -m poetry env info --path)/bin/activate
 
 streamlit run webui.py
